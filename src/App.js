65e73d1ac0022200d3e0b0d5313243e15593e67e
michalmilunas <michalmilunas@o2.pl>
2021-01-23 12:36:56 +0100

    Added homepage route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import HomePage from './HomePage'
 
 function App() {
   return (
-    <form class="login-form" action="javascript:void(0);">
+  <Router>
+  <Switch>
+    <Route exact path="/">
+      <LoginForm/>
+    </Route>
+    <Route path="/HomePage">
+      <HomePage />
+    </Route>
+  </Switch>
+  </Router>
+  );
+  
+}
+
+function LoginForm() {
+  return (<form class="login-form" action="javascript:void(0);">
     <h1>Logowanie</h1>
     <div class="form-input-material">
       <input type="input" class="form__field" placeholder="Nazwa użytkownika" name="name" id='name' required />
     </div>
     <div class="form-input-material">
       <input type="input" class="form__field" placeholder="Hasło" name="password" id='password' required />
-    </div>
-    <button type="submit" class="btn btn-primary btn-ghost">Zaloguj się</button>
+    </div>   
+    <Link to="/HomePage">
+        <button type="submit" class="btn btn-primary btn-ghost">Zaloguj się</button>
+    </Link>
   </form>
-  );
+  )
 }
+
 
 export default App;
